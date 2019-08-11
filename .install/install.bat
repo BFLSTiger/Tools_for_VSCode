a0fefe65de98a89c682ca521b63c194dec2d6c1f
BFLSTiger <sannianjiaoshumayi@126.com>
2019-08-11 14:59:46 +0800

    fix

--- .install/install.bat
+++ .install/install.bat
@@ -3,7 +3,7 @@ echo Compiling the file "compile.cpp"
 g++ compile.cpp -o compile -Ofast -std=c++11 -Wl,--stack=1024000000
 echo Compiling the file "run.cpp"
 g++ run.cpp -o run -Ofast -std=c++11 -Wl,--stack=1024000000
-echo moving the files
+echo Moving the files
 cd ../ && mkdir scripts
 cd .install
 move compile.exe ../scripts/
