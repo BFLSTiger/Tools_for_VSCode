ee831cb8d4a3237f1f4844bd8d71da804349751a
BFLSTiger <sannianjiaoshumayi@126.com>
2025-12-18 05:15:06 +0800

    Update a new version with extension.

--- .install/install.bat
+++ .install/install.bat
@@ -1,12 +1,8 @@
 @echo off
-echo Compiling the file "compile.cpp"
-g++ compile.cpp -o compile -Ofast -std=c++11 -Wl,--stack=1024000000 -static
 echo Compiling the file "run.cpp"
 g++ run.cpp -o run -Ofast -std=c++11 -Wl,--stack=1024000000 -static
 echo Moving the files
 cd ../ && mkdir scripts
 cd .install
-move compile.exe ../scripts/
 move run.exe ../scripts/
-echo Installed successfully!
-pause
+code --install-extension ./extension.vsix
--- .install/run.cpp
+++ .install/run.cpp
@@ -1,36 +1,44 @@
-#include<cstdio>
-#include<iostream>
-#include<cstring>
-#include<algorithm>
-#include<cstdlib>
-#include<string>
-#include<ctime>
+#define opt "-Wl,--stack=1024000000 -O2 -std=c++11 -Wall -Wextra"
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cstdlib>
+#include <string>
+#include <ctime>
 using namespace std;
 namespace Dango
 {
-	string cmd,tmp[5];
-	int work(int argc,char **argv)
+	string cmd;
+	int work(int argc, char **argv)
 	{
-		for(int i=0;i<argc;i++)
-			tmp[i]=argv[i];
-		if(tmp[3]==".py")cmd="cd /D "+tmp[1]+" && python "+tmp[2]+tmp[3];
-		else if (tmp[3]==".bat")cmd="cd /D "+tmp[1]+" && "+tmp[2]+tmp[3];
-		else cmd="cd /D "+tmp[1]+" && "+tmp[2];
-		cout<<"----Programe Started----"<<endl;
-		double start=clock();
-		int res=system(cmd.c_str());
-		double end=clock();
-		cout<<endl;
-		if(res)
+		if (argc > 2)
 		{
-			cout<<"Runtime Error with exit code "<<res<<endl;
+			cmd = argv[2];
+			if (system(cmd.c_str()))
+			{
+				cout << "Compile Error." << endl;
+				return -1;
+			}
+			cout << "----Compile Success----" << endl;
+		}
+		cmd = argv[1];
+		cout << "----Programe Started----" << endl;
+		double start = clock();
+		int res = system(cmd.c_str());
+		double end = clock();
+		cout << endl;
+		if (res)
+		{
+			cout << "Runtime Error with exit code " << res << endl;
 			return 0;
 		}
-		else cout<<"Time Used: "<<(end-start)/1000<<"s"<<endl;
+		else
+			cout << "Time Used: " << (end - start) / CLOCKS_PER_SEC << "s" << endl;
 		return 0;
 	}
 }
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
-	return Dango::work(argc,argv);
+	return Dango::work(argc, argv);
 }
